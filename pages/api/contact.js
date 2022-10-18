149e0968bca1d8bfa7263da5e2572ec798b946a7
zippi-wzp <truebatmandc@gmail.com>
2022-10-18 20:36:58 +0300

    added next.config.js and optimized

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -25,10 +25,10 @@ async function handler(req, res) {
         };
 
         let client;
+        const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.fvnnlct.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
         try {
-            client = await MongoClient.connect(
-                "mongodb+srv://artem_yeryshev:7BVnnSyBXnTAT1dm@cluster0.fvnnlct.mongodb.net/my-site?retryWrites=true&w=majority"
-            );
+            client = await MongoClient.connect(connectionString);
 
         } catch(error) {
             res.status(500).json({ message: "Could not connect to database."});
