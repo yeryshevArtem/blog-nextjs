a01da0a999bdfb2b149d3c292ebdc9c38a7100c8
zippi-wzp <truebatmandc@gmail.com>
2022-10-18 16:29:14 +0300

    added notifications.

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,28 +1,90 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Notification  from "../ui/notification";
 import classes from "./contact-form.module.css";
+
+async function sendContactData(contactDetails) {
+    const response = await fetch('/api/contact', {
+        method: "POST",
+        body: JSON.stringify(contactDetails),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || "Something went wrong!");
+    }
+
+    return data;
+}
 
 function ContactForm() {
     const [enteredEmail, setEnteredEmail] = useState('');
     const [enteredName, setEnteredName] = useState('');
     const [enteredMessage, setEnteredMessage] = useState('');
+    const [requestStatus, setRequestStatus] = useState();
+    const [requestError, setRequestError] = useState();
+
+    useEffect(() => {
+        let timer;
+        if (requestStatus === "success" || requestStatus === "error") {
+            timer = setTimeout(() => {
+                setRequestStatus(null);
+                setRequestError(null);
+            }, 3000);
+        }
+        return () => clearTimeout(timer);
+    }, [requestStatus])
     
-    function sendMessageHandler(event) {
+    async function sendMessageHandler(event) {
         event.preventDefault();
 
-        // add client side validation
+        setRequestStatus("pending");
+        try {
 
-        fetch('/api/contact', {
-            method: "POST",
-            body: JSON.stringify({
+            await sendContactData({
                 email: enteredEmail,
                 name: enteredName,
                 message: enteredMessage
-            }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
+            });
+            setRequestStatus("success");
+            setEnteredEmail('');
+            setEnteredMessage('');
+            setEnteredName('');
+        } catch(err) {
+            setRequestError(err.message);
+            setRequestStatus("error");
+        }
     }
+
+    let notification;
+
+    if (requestStatus === "pending") {
+        notification = {
+            status: "pending",
+            title: "Sending message...",
+            message: "Your message is on its way!"
+        }
+    }
+
+    if (requestStatus === "success") {
+        notification = {
+            status: "success",
+            title: "Success!",
+            message: "Message sent successfully!"
+        }
+    }
+
+    if (requestStatus === "error") {
+        notification = {
+            status: "error",
+            title: "Error!",
+            message: requestError
+        }
+    }
+
     return (
         <section className={classes.contact}>
             <h1>How can I help you?</h1>
@@ -63,6 +125,13 @@ function ContactForm() {
                     <button>Send Message</button>
                 </div>
             </form>
+            {notification && (
+                <Notification 
+                    status={notification.status}
+                    title={notification.title}
+                    message={notification.message}
+                />
+            )}
         </section>
     );
 }
