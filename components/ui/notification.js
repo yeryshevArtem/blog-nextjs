58209de02c87963e781a47ec35e563d09041542e
zippi-wzp <truebatmandc@gmail.com>
2022-10-18 17:44:35 +0300

    added notification portal

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import classes from './notification.module.css';
@@ -17,12 +18,12 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
-  );
+    </div>, 
+  document.getElementById("notifications"));
 }
 
 export default Notification;
